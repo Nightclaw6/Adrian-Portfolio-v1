f7c2430524279b13b26557920e2c36213706fb55
Nightclaw6 <sclaudiu82@yahoo.com>
2022-03-05 23:58:40 -0500

    first commit

---
+++ loader.js
@@ -0,0 +1,6 @@
+const loader = document.querySelector('.center'); // center the parent name as you see it in the tuto
+window.addEventListener('load', () =>{    
+    setTimeout(function () {
+        loader.style.display = "none";
+    },1500)
+})
