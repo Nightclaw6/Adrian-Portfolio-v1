d1ca433560045d45eaf1c94d63725c90fe443874
Nightclaw6 <sclaudiu82@yahoo.com>
2022-05-31 19:50:27 -0400

    fixed bugs

--- hamburger.js
+++ hamburger.js
@@ -3,6 +3,7 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+const navLinks = document.querySelectorAll('.nav-link');
 
 let showMenu = false;
 
@@ -11,17 +12,33 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
     if (!showMenu) {
         menuBtn.classList.add('close');
+        menuBtn.classList.add('hideBtn');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach( item => item.classList.add('show'))
         showMenu = true;
+        navLinks.forEach( link => link.addEventListener('click', closeMenu));
     } else {
         menuBtn.classList.remove('close');
+        menuBtn.classList.add('hideBtn');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        navItems.forEach( item => item.classList.remove('show'))
+        navItems.forEach( item => item.classList.remove('show'));
         showMenu = false;
+        navLinks.forEach( link => link.addEventListener('click', closeMenu));
     }
 }
+
+function closeMenu() {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach( item => item.classList.remove('show'));
+    showMenu = false;
+}
+
+
+
